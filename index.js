b1cff075deb562b9cb871046423be9a83bdd2840
Nick Zelei <nickzelei@gmail.com>
2017-01-03 20:58:07 -0600

    commander updates

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+#!/usr/bin/env node
 const commander = require("commander");
+const parser = require("./parser");
 
 commander
     .version("0.0.0")
-    .option("-f", "--file", "Parse a single file.")
-    .option("-d", "--directory", "Parse an entire directory of mp4 files")
-    .option("-o", "--output", "Specify a directory to output the mp4. Defaults to the input location")
+    .option("-f, --file [file]", "Parse a single file.")
+    .option("-d, --directory", "Parse an entire directory of mp4 files")
+    .option("-o, --output", "Specify a directory to output the mp4. Defaults to the input location")
     .parse(process.argv);
 
+if (commander.file) {
+    throw new Error("Not yet implemented");
+}
+
+if (commander.directory) {
+    throw new Error("Not yet implemented");
+}
